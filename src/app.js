503b1133763b1960e815109ce22548d6b538aa62
Aref2880 <Aref.delshab2880@gmail.com>
2024-05-26 14:29:56 +0330

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import BMIcalculator from './BMIcalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BMIcalculator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
